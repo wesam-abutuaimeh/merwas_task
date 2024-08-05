569c3276ed880b2abe60231ec1fdb90ca46d5ebf
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2024-08-05 18:14:45 +0300

    comman style and constans

---
+++ src/constans/inputs.js
@@ -0,0 +1,22 @@
+export const INPUTS = [
+  {
+    id: "types",
+    class: "types",
+    label: "Types",
+    type: "text",
+    placeholder: "complain",
+  },
+  {
+    id: "name",
+    class: "name",
+    label: "Name",
+    type: "text",
+    placeholder: "ali",
+  },
+  {
+    id: "email",
+    class: "email",
+    label: "Email",
+    type: "email",
+  },
+];
