998dff82b4b59abf7ee01ad3450ba0e23e788253
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2024-08-06 15:33:41 +0300

    Remove objectFit propirety

--- src/components/atoms/CustomImage/index.jsx
+++ src/components/atoms/CustomImage/index.jsx
@@ -2,15 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 function CustomImage({ imgSrc, width, height, title }) {
-  return (
-    <Image
-      src={imgSrc}
-      width={width}
-      height={height}
-      alt={title}
-      objectFit="Contain"
-    />
-  );
+  return <Image src={imgSrc} width={width} height={height} alt={title} />;
 }
 
 export default CustomImage;
