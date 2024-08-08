2a78fc1260f3d20b13e706ffc4c61ac127a52e5f
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2024-08-08 18:31:35 +0300

    push on branch 2 for test

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -2,7 +2,6 @@ import LandingSection from "@/components/sections/LandingSection";
 import Features from "@/components/sections/Features";
 import Header from "@/components/organisms/Header";
 import Partners from "@/components/sections/Partners";
-import ContactUs from "@/components/sections/ContactUs";
 import Footer from "@/components/organisms/Footer";
 import AboutUs from "@/components/sections/AboutUs";
 import Services from "../sections/Services";
@@ -16,7 +15,7 @@ export default function HomePage() {
       <AboutUs />
       <Services />
       <Partners />
-      <ContactUs />
+
       <Footer />
     </main>
   );
